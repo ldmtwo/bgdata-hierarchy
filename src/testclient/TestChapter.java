48ca4a36168b84008e25913a017735c80f01bdf1
LDMtwo@gmail.com <LDMtwo@gmail.com@aac92548-06a9-c836-4b77-b6ad57b619c7>
2013-10-29 19:13:58 +0000

    Version 1

---
+++ src/testclient/TestChapter.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testclient;
+
+import java.io.IOException;
+import storageserver.Dictionary;
+
+/**
+ *
+ * @author ldtwo
+ */
+public class TestChapter {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Dictionary book = new Dictionary();
+        
+    }
+}
